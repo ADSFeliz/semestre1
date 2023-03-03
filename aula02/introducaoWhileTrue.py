861b59ee04c4b226abb326f8f4b96e4a21f90f78
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-03-03 23:29:57 +0000

    feat : exemplos da aula

---
+++ aula02/introducaoWhileTrue.py
@@ -0,0 +1,7 @@
+um = "Viver, viver e ser livre"
+dois = "Saber dar valor para as coisas mais simples"
+tres = "Só o amor constrói"
+quatro = "Pontes indestrutíveis"
+
+while True:
+    print(um,dois,tres,quatro)
