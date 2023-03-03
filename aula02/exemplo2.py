ead6933140968c2c9a1375ef7bf3ffdd5747171f
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-03-03 23:30:40 +0000

    feat : definindo a tipagem

--- aula02/exemplo2.py
+++ aula02/exemplo2.py
@@ -1,6 +1,6 @@
 nome = input("Digite o seu nome: ")
 idade = int(input("Digite a sua idade: ")) # Definindo como int a variavel = somente numeros inteiros
-altura = (float(input("Qual é a sua altura?")))# Definindo como float a variavel = numeros com virgula
+altura = (float(input("Qual é a sua altura?")))# Definindo como float a variavel = numeros com ponto
 anoIngresso = 2023
 
 print(f"Olá, {nome}. Você tem {idade} anos. Você mede {altura} m")
