71e3addc2bf354859eb78cb8ffdae31ab4ef3038
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-03-18 03:10:41 +0000

    fix : realizado o mesmo jogo, mas com funções

--- aula04/joKenPo.py
+++ aula04/joKenPo.py
@@ -1,46 +1,41 @@
 import random
+
 class bcolors:
-    JG1 = '\033[92m' #GREEN
-    JG2 = '\033[93m' #YELLOW
-    RESET = '\033[0m' #RESET COLOR
+    JG1 = '\033[92m' #VERDE
+    JG2 = '\033[93m' #AMARELO
+    RESET = '\033[0m' #RESETAR COR
 
+def jogar(jogada_jogador_1, jogada_jogador_2):
+    if jogada_jogador_1 == jogada_jogador_2:
+        return "Empate", 0
+    elif (jogada_jogador_1 == 1 and jogada_jogador_2 == 3) or \
+         (jogada_jogador_1 == 2 and jogada_jogador_2 == 1) or \
+         (jogada_jogador_1 == 3 and jogada_jogador_2 == 2):
+        return "Resultado partida: Jogador 1", 1
+    else:
+        return "Resultado partida: Jogador 2", 2
 
-pontosJg1,pontosJg2=0,0
-pedra, papel, tesoura = 1,2,3
+def resultadoJogo(pontos_jogador_1, pontos_jogador_2):
+    if pontos_jogador_1 > pontos_jogador_2:
+        return bcolors.JG1 + "Resultado melhor de três: Jogador 1" + bcolors.RESET
+    elif pontos_jogador_2 > pontos_jogador_1:
+        return bcolors.JG2 + "Resultado melhor de três: Jogador 2" + bcolors.RESET
+    else:
+        return "Resultado melhor de três: Empate"
 
+pontos_jogador_1, pontos_jogador_2 = 0, 0
 cont = 0
-while cont<3:  
-    jg1 = int(input("Informe a sua jogada: (1) Pedra (2) Papel (3) Tesoura: "))
-    jg2 = random.randint(1,3)
+while cont < 3: 
+    jogada_jogador_1 = int(input("Informe a sua jogada: (1) Pedra (2) Papel (3) Tesoura: "))
+    jogada_jogador_2 = random.randint(1, 3)
+    
+    resultado, ponto = jogar(jogada_jogador_1, jogada_jogador_2)
+    print(resultado)
+    pontos1, pontos2 = 0, 0
+    if ponto == 1:
+        pontos_jogador_1 += 1
+    elif ponto == 2:
+        pontos_jogador_2 += 1
+    cont += 1
 
-    cont+=1
-    if(jg1==jg2):
-        print("Empate")
-    if jg1==pedra:
-        if jg2==papel:
-            print("jg2 Venceu")   
-            pontosJg2+=1         
-        else:
-            print("jg1 Venceu")
-            pontosJg1+=1       
-    if jg1==papel:
-        if jg2==pedra:
-            print("jg1 Venceu")
-            pontosJg1+=1
-        else:
-            print("jg2 Venceu")
-            pontosJg2+=1
-    if jg1==tesoura:
-        if jg2==papel:
-            print("jg1 Venceu")
-            pontosJg1+=1
-        elif jg2==pedra:
-            print("jg2 Venceu")
-            pontosJg2+=1
-
-if pontosJg1>pontosJg2:
-    print(bcolors.JG1 + "jg1 Venceu melhor de três!" + bcolors.RESET)
-elif pontosJg2>pontosJg1:  
-    print(bcolors.JG2 + "jg2 Venceu melhor de três!" + bcolors.RESET)
-else:
-    print("Empatou melhor de três!" )
+print(resultadoJogo(pontos_jogador_1, pontos_jogador_2))
