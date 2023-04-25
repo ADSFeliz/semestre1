452df7389c3c88743a932796399b1143dcab33d5
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-04-25 22:34:09 +0000

    fix : Estava com um erro de sintaxe

--- provas/certificacao/ex4.py
+++ provas/certificacao/ex4.py
@@ -5,16 +5,15 @@ maiores=[]
 menores=[]
 soma=0
 for i in range(0,100):
-    numero = random(0,1000)
-    numeros.add(numero)
+    numero = random.randint(0,1000)
+    numeros.append()(numero)
     soma+=numero
 
 media = soma/(len(numeros))
 for i in range(len(numeros)):
     if numeros[i]>media:
-        maiores.add(numeros[i])
+        maiores.append(numeros[i])
     if numeros[i]<media:
-        menores.add(numeros[i])
+        menores.append(numeros[i])
 
 print(f"Media: {media} Maiores:{len(maiores)} Menores:{len(menores)}")
-
