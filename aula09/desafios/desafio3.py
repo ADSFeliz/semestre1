c57fae119ca352235dc5add7273a24a616b82b83
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-05-12 23:33:55 +0000

    fix : ajuste maxsize

--- aula09/desafios/desafio3.py
+++ aula09/desafios/desafio3.py
@@ -3,7 +3,7 @@ import sys
 n1_n2 = input('Número 1 e número 2: ')
 numeros = n1_n2.split()
 n1,n2= int(numeros[0]),int(numeros[1])
-menor_inteiro = -99999
+menor_inteiro = -1*sys.maxsize
 
 for n3 in range(n1,menor_inteiro,-1):
     media = (n1+n2+n3)/3
