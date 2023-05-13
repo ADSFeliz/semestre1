7819b10126a3fc43d43864e9316dcb0b53a1763e
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-05-13 00:31:20 +0000

    fix : otimização

--- aula09/desafios/desafio3.py
+++ aula09/desafios/desafio3.py
@@ -1,12 +1,6 @@
-import sys
-
 n1_n2 = input('Número 1 e número 2: ')
 numeros = n1_n2.split()
 n1,n2= int(numeros[0]),int(numeros[1])
-menor_inteiro = -1*sys.maxsize
 
-for n3 in range(n1,menor_inteiro,-1):
-    media = (n1+n2+n3)/3
-    if(media == n1):
-        print(n3)
-        break
+n3 = (n1*2)-n2
+print(n3)
